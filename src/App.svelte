cb9ad7a27e58f3ce34246e4738a9c8fe646d1049
David Benner <davebenner14@gmail.com>
2023-11-17 12:51:28 -0500

    Add basic wordle functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,91 @@
 <script>
-	export let name;
+  import { onMount } from 'svelte';
+
+  let words = [];
+  let currentWord = '';
+  let currentGuess = '';
+  let guesses = [];
+  const maxGuesses = 6;
+  const wordLength = 5;
+
+  // Fetch words and select a random one on component mount
+  onMount(async () => {
+    const res = await fetch('/words.txt');
+    const text = await res.text();
+    words = text.split('\n').filter(Boolean);
+    currentWord = selectRandomWord();
+  });
+
+  function selectRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  function handleInput(event) {
+    if (currentGuess.length < wordLength && /^[A-Za-z]$/.test(event.key)) {
+      currentGuess += event.key.toUpperCase();
+    }
+    if (event.key === 'Enter' && currentGuess.length === wordLength) {
+      submitGuess();
+    }
+    if (event.key === 'Backspace') {
+      currentGuess = currentGuess.slice(0, -1);
+    }
+  }
+
+  function submitGuess() {
+    guesses.push(currentGuess);
+    currentGuess = '';
+  }
+
+  function getCellColor(row, index) {
+    const letter = guesses[row]?.[index];
+    if (!letter) return '';
+    if (letter === currentWord[index]) return 'green';
+    if (currentWord.includes(letter)) return 'yellow';
+    return 'grey';
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:window on:keydown={handleInput} />
+
+<div class="grid">
+  {#each Array(maxGuesses) as _, row}
+    <div class="row">
+      {#each Array(wordLength) as _, col}
+        <div class="cell {getCellColor(row, col)}">
+          {guesses[row]?.[col] || ''}
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .grid {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .row {
+    display: flex;
+  }
+  .cell {
+    width: 50px;
+    height: 50px;
+    border: 1px solid #ddd;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2em;
+    margin: 5px;
+  }
+  .green {
+    background-color: green;
+  }
+  .yellow {
+    background-color: yellow;
+  }
+  .grey {
+    background-color: grey;
+  }
 </style>
